fb9a515cccb0b83500ad23f6ed87e9018449458a
MR.B7348 <91031584+oofboythegreat@users.noreply.github.com>
2026-03-21 12:13:25 -0700

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -3,5 +3,7 @@ public class Enemy extends GameObject {
     public Enemy(int x, int y, int width, int height, String imagePath){
         super(x, y, width, height, imagePath);
     }
-    
+    //public void move(){
+        
+    //}
 }
